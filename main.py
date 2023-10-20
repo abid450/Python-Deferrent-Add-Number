b1cf378bc22767bbd96cf0e2aff0aca3d6611233
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-10-20 21:44:01 +0600

    I have a upload Python file

---
+++ main.py
@@ -0,0 +1,40 @@
+
+# Deferent add of Number----------------
+
+num1 = 20
+num2 = 40
+
+result = 20 + 40
+print ("The Sum is : ",result)
+
+result = 60 - 40
+print ("The Minus is : ",result)
+
+result = 70 * 40
+print ("The into is : ",result)
+
+result = 80 / 40
+print ("The Devition is : ",result)
+
+
+
+# Deferent add of types number---------------
+
+num1 = int (input("Enter First Number : "))
+num2 = int (input("Enter Second Number : "))
+
+result = num1 + num2
+print ("The Sum is : ",result)
+
+result = num1 - num2
+print ("The Minus is : ",result)
+
+result = num1 * num2
+print ("The into is : ",result)
+
+result = num1 / num2
+print ("The Devition is : ",result)
+
+
+
+
